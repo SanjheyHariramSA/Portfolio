6eefef8eb7e366f148985bc85187192ac994eba7
SANJHEY HARIRAM SA <sanjheyhariramcse1923@gmail.com>
2023-06-23 13:45:38 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,78 @@
+/*Function for Github link to Open in New Window*/
+function open_new_G(){
+    window.open("https://github.com/SanjheyHariramSA");
+}
+/*Function for LinkedIn link to open in New Window */
+function open_new_LI(){
+    window.open("https://www.linkedin.com/in/sanjhey-hariram-sa-710a19212");
+}
+/*Function to Open TIC TAC TOE Source Code */
+function TicTacToe_S(){
+    window.open("https://github.com/SanjheyHariramSA/Tic-Tac-Toe.git");
+}
+/*Function to Open REAL TIME CHAT APP Source Code */
+function RTCA_S(){
+    window.open("https://github.com/SanjheyHariramSA/Real_Time_Chat_App.git");
+}
+/*Function to Open E-VOTING SYSTEM Source Code */
+function E_Voting_S(){
+    window.open("https://github.com/SanjheyHariramSA/Secured-Electronic-Voting-System-with-Appropriate-Authentication-Using-Blockchain.git");
+}
+/*Download PDF from Google Drive */
+const download_btn=document.getElementById('btn');
+download_btn.addEventListener('click',download_file());
+function download_file(){
+    const fileUrl='https://drive.google.com/file/d/1_ltwqfNzdOlv2kRsbpc8I9AhiD7Kpnxr/view?usp=drive_link';
+    window.open(fileUrl, '_blank');
+}
+/*Download Course Certificate */
+const download_html_c=document.getElementById('html');
+download_html_c.addEventListener('click',download_html());
+function download_html(){
+    const fileUrl='https://drive.google.com/file/d/1yd7oPJKdnwB-tG-HwWHGai83r410zzam/view?usp=drive_link';
+    window.open(fileUrl, '_blank');
+}
+const download_css_c=document.getElementById('css');
+download_css_c.addEventListener('click',download_css());
+function download_css(){
+    const fileUrl='https://drive.google.com/file/d/16nvcTnFKy8uXIIjilbwffbil1gC-B8q8/view?usp=drive_link';
+    window.open(fileUrl, '_blank');
+}
+
+const download_js_c=document.getElementById('js');
+download_html_c.addEventListener('click',download_js());
+function download_js(){
+    const fileUrl='https://drive.google.com/file/d/1-70ixXHjHwDwEGY_-Lfip2oeHMc9Ev_7/view?usp=drive_link';
+    window.open(fileUrl, '_blank');
+}
+const download_react_c=document.getElementById('react');
+download_html_c.addEventListener('click',download_react());
+function download_react(){
+    const fileUrl='https://drive.google.com/file/d/1Yy5W0IeUka7wYsG2rxKflOMhCuYHhVTb/view?usp=drive_link';
+    window.open(fileUrl, '_blank');
+}
+const download_java_c=document.getElementById('java');
+download_html_c.addEventListener('click',download_java());
+function download_java(){
+    const fileUrl='https://drive.google.com/file/d/1hhHSiBfENNN_lt-jE8FYUkeW7z3TupiJ/view?usp=drive_link';
+    window.open(fileUrl, '_blank');
+}
+/*Download Intership Summary Report */
+const download_lgm_c=document.getElementById('lgm');
+download_lgm_c.addEventListener('click',download_lgm());
+function download_lgm(){
+    const fileUrl='https://drive.google.com/file/d/1feU_epxKyOtXJZANzdErfk-nfi9WsAEP/view?usp=drive_link';
+    window.open(fileUrl, '_blank');
+}
+const download_ten_c=document.getElementById('ten');
+download_lgm_c.addEventListener('click',download_ten());
+function download_ten(){
+    const fileUrl='https://drive.google.com/file/d/18u-Ie3NZ_bqSUqFyuw8Bz3cCLQiHKvaZ/view?usp=drive_link';
+    window.open(fileUrl, '_blank');
+}
+const download_ivtl_c=document.getElementById('ivtl');
+download_lgm_c.addEventListener('click',download_ivtl());
+function download_ivtl(){
+    const fileUrl='https://drive.google.com/file/d/1RcMl_rl4wx8fxFBO6Um42tB8ZkluJiea/view?usp=drive_link';
+    window.open(fileUrl, '_blank');
+}
